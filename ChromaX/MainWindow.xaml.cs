c3c7b10d0521e560d15784d4c0aa1afb8561ae49
Long Tran <lngtrn94@gmail.com>
2018-10-31 01:13:29 +0100

    add color picker

--- ChromaX/MainWindow.xaml.cs
+++ ChromaX/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Media;
+using ChromaX.Model;
+using Xceed.Wpf.Toolkit;
 
 namespace ChromaX
 {
@@ -10,6 +14,24 @@ namespace ChromaX
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ColorPicker.AvailableColors.Clear();
+            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.White.Color, "White"));
+            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.Black.Color, "Black"));
+            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.Red.Color, "Red"));
+            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.Green.Color, "Green"));
+            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.Blue.Color, "Blue"));
+            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.Cyan.Color, "Cyan"));
+            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.Magenta.Color, "Magenta"));
+            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.Yellow.Color, "Yellow"));
+            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.HotPink.Color, "HotPink"));
+            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.Orange.Color, "Orange"));
+            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.Purple.Color, "Purple"));
+
+            ColorPicker.SelectedColor = ChromaColor.White.Color;
         }
     }
 }
