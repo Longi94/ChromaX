c684f005dba0f65c891676735d8da4c2c58d7656
Long Tran <lngtrn94@gmail.com>
2018-11-03 16:12:41 +0100

    mvvm color picker

--- ChromaX/MainWindow.xaml.cs
+++ ChromaX/MainWindow.xaml.cs
@@ -13,23 +13,5 @@ namespace ChromaX
         {
             InitializeComponent();
         }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            ColorPicker.AvailableColors.Clear();
-            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.White.Color, "White"));
-            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.Black.Color, "Black"));
-            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.Red.Color, "Red"));
-            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.Green.Color, "Green"));
-            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.Blue.Color, "Blue"));
-            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.Cyan.Color, "Cyan"));
-            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.Magenta.Color, "Magenta"));
-            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.Yellow.Color, "Yellow"));
-            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.HotPink.Color, "HotPink"));
-            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.Orange.Color, "Orange"));
-            ColorPicker.AvailableColors.Add(new ColorItem(ChromaColor.Purple.Color, "Purple"));
-
-            ColorPicker.SelectedColor = ChromaColor.White.Color;
-        }
     }
 }
--- ChromaX/ViewModel/MainWindowViewModel.cs
+++ ChromaX/ViewModel/MainWindowViewModel.cs
@@ -1,5 +1,10 @@
-﻿using System.Windows.Input;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Windows.Media;
+using ChromaX.Model;
 using ChromaX.Service;
+using Xceed.Wpf.Toolkit;
 
 namespace ChromaX.ViewModel
 {
@@ -7,6 +12,15 @@ namespace ChromaX.ViewModel
     {
         private readonly ChromaService _chromaService;
 
+        public MainWindowViewModel(ChromaService chromaService)
+        {
+            _chromaService = chromaService;
+            _chromaService.SdkInit += (sender, eventArgs) => SdkInitialized = eventArgs.Initialized;
+
+            AddAvailableColors();
+        }
+
+        #region Status Bar
 
         private bool _sdkInitialized;
 
@@ -50,12 +64,6 @@ namespace ChromaX.ViewModel
             }
         }
 
-        public MainWindowViewModel(ChromaService chromaService)
-        {
-            _chromaService = chromaService;
-            _chromaService.SdkInit += (sender, eventArgs) => SdkInitialized = eventArgs.Initialized;
-        }
-
         private void ConnectSdk(object obj)
         {
             _chromaService.Initialize();
@@ -65,5 +73,42 @@ namespace ChromaX.ViewModel
         {
             _chromaService.UnInitialize();
         }
+
+        #endregion
+
+        #region Color Picker
+
+        private Color _selectedColor = ChromaColor.White.Color;
+
+        public Color SelectedColor
+        {
+            get => _selectedColor;
+            set
+            {
+                _selectedColor = value;
+                OnPropertyChanged(nameof(SelectedColor));
+            }
+        }
+
+        private readonly ObservableCollection<ColorItem> _availableColors = new ObservableCollection<ColorItem>();
+
+        public IEnumerable<ColorItem> AvailableColors => _availableColors;
+
+        private void AddAvailableColors()
+        {
+            _availableColors.Add(new ColorItem(ChromaColor.White.Color, "White"));
+            _availableColors.Add(new ColorItem(ChromaColor.Black.Color, "Black"));
+            _availableColors.Add(new ColorItem(ChromaColor.Red.Color, "Red"));
+            _availableColors.Add(new ColorItem(ChromaColor.Green.Color, "Green"));
+            _availableColors.Add(new ColorItem(ChromaColor.Blue.Color, "Blue"));
+            _availableColors.Add(new ColorItem(ChromaColor.Cyan.Color, "Cyan"));
+            _availableColors.Add(new ColorItem(ChromaColor.Magenta.Color, "Magenta"));
+            _availableColors.Add(new ColorItem(ChromaColor.Yellow.Color, "Yellow"));
+            _availableColors.Add(new ColorItem(ChromaColor.HotPink.Color, "HotPink"));
+            _availableColors.Add(new ColorItem(ChromaColor.Orange.Color, "Orange"));
+            _availableColors.Add(new ColorItem(ChromaColor.Purple.Color, "Purple"));
+        }
+
+        #endregion
     }
 }
